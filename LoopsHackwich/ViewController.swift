482a48c5ec50776afcdc5b365dee46d4aba9375e
EDUARDO MENDOZA <rl238513@rlas-macair-25.local>
2019-01-10 11:53:53 -0600

    suicide

--- LoopsHackwich/ViewController.swift
+++ LoopsHackwich/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    var fruitArray = ["Apples", "Banana", "Pears", "Grapes"]
+    
+    var vegetablesArray = ["Onions", "Garlic", "Brocoli", "Squash"]
+    
+    var memeArray = ["SpongeGar", "Big Chungus", "Do It", "Hello There"]
+    
+    var snackArray = ["Chips", "Popcorn", "Peanuts", "Candy"]
+    
+    var addressDictionary = ["nama": "Tom", "Address": "123 Fake Street", "zip": "123456"]
+    
+    var output = " "
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func arrayOne(_ sender: Any) {
+        for fruit in fruitArray{
+            print(fruit)
+            output += "\(fruit)\n"
+        }
+        textView.text = output
+    }
+    
+    @IBAction func arrayTwo(_ sender: Any) {
+        for i in 0...3{
+            print(i)
+            output += "\(vegetablesArray[i])\n"
+        }
+        textView.text = output
+    }
+    
+    @IBAction func arrayThree(_ sender: Any) {
+    }
+    
+    @IBAction func arrayFour(_ sender: Any) {
+    }
+    
+    @IBAction func dictionary(_ sender: Any) {
+    }
+    
 }
 
